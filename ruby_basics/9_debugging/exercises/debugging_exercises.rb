91c66ba6920d7ba16e3653a361b724c54ab5a809
Ricardo Reyes <HDRickstar@gmail.com>
2026-05-06 16:02:00 -0500

    hi

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,4 +1,3 @@
-require 'pry-byebug'
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -53,8 +52,7 @@ end
 def yell_greeting(string)
   name = string
 
-  binding.pry
-  
+
   name = name.downcase
   greeting = "WASSAP, #{name}!"
 end
